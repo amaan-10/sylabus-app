daff5db43c1cce44319e60bcad24fd039aa71d67
Amaan <amaanshaikh.gg@gmail.com>
2025-11-28 23:51:56 +0530

    add: Hero, About component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import About from "@/components/About";
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <About />
     </div>
   );
 }
