d8a97a0333711ac0c0e48830b4d8e54dce5f896d
Amaan <amaanshaikh.gg@gmail.com>
2025-11-29 23:58:05 +0530

    add: Services, Benefits, Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import About from "@/components/About";
+import Benefits from "@/components/Benefits";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col place-content-start items-center gap-40 h-min min-h-screen w-auto p-0 relative overflow-visible">
       <Navbar />
       <Hero />
       <About />
+      <Services />
+      <Benefits />
+      <Footer />
     </div>
   );
 }
