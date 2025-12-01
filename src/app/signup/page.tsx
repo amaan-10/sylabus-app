4598bb1d26a84cd7ba3e1bdc771500a2a7fbfef7
Amaan <amaanshaikh.gg@gmail.com>
2025-12-01 23:23:23 +0530

    add: page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import Register from "@/components/Register";
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default SignUpPage;
