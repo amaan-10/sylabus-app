b36c34697256372630b5bb5dbbeeec7e02160f06
Amaan <amaanshaikh.gg@gmail.com>
2026-01-07 01:43:15 +0530

    update: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,9 +23,9 @@ export function middleware(req: NextRequest) {
 
   // Redirect logged-in users away from auth pages
   if (
-    session &&
-    // pathname === "/" ||
-    (pathname === "/signin" || pathname === "/signup")
+    (session && pathname === "/") ||
+    pathname === "/signin" ||
+    pathname === "/signup"
   ) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
